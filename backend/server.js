df2c9317663f09031be0506f6fa316ba096b7d42
babureddyg2308 <158761114+babureddyg2308@users.noreply.github.com>
2024-06-06 17:47:07 +0530

    backend-get

--- backend/server.js
+++ backend/server.js
@@ -12,5 +12,13 @@ connectDB();
 app.use(bodyParser.json());
 app.use('/api/books', bookRoutes);
 
+ app.get('/', (req, res) => {
+    res.send('Backend server is running');
+});
+
+ app.get('/health', (req, res) => {
+    res.send({ status: 'UP' });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
